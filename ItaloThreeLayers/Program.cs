4ac6938f26cc87055ed907923144a2a738d1b93a
MartaPetruzzelliMSC <E-marta.petruzzelli@msc.com>
2025-10-07 17:57:57 +0200

    italo half 2

---
+++ ItaloThreeLayers/Program.cs
@@ -0,0 +1,32 @@
+﻿using Business;
+using DataAccess;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Presentation;
+
+namespace ItaloThreeLayers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var applicationBuilder = Host.CreateApplicationBuilder();
+            applicationBuilder.Services.AddTransient<IBookingRepository, BookingJsonRepository>();
+            applicationBuilder.Services.AddSingleton<IBookingConverter, BookingConverter>();
+            applicationBuilder.Services.AddSingleton<IBookingService, BookingService>();
+            applicationBuilder.Services.AddSingleton<IConsoleView, ConsoleView>();
+
+            using var host = applicationBuilder.Build();
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            //IBookingRepository bookingRepository = new BookingRepository();
+            //IBookingConverter bookingConverter = new BookingConverter();
+            //IBookingService bookingService = new BookingService(bookingRepository, bookingConverter);
+            var consoleView = serviceScope.ServiceProvider.GetService<IConsoleView>();
+            if(consoleView != null)
+            {
+                consoleView.ShowMenu();
+            }
+
+        }
+    }
+}
